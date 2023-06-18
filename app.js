f69ef34f4214448ac9d2c635665303709b045adf
Mauricio3697 <133618458+Mauricio3697@users.noreply.github.com>
2023-06-18 12:59:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,24 +6,7 @@
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
         })
-  function ativaletra(elemento){
-    const arrTexto = elemento.innerHTML.split('')
-    elemento.innerHTML = '';
-    arrTexto.forEach((letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML+= letra;
-       }, 75 * i)
-    });
-    }
- 
-    
-const titulo = document.querySelector('.digitando');
-ativaletra(titulo);
 
-const ativamenu = document.querySelector('.fa-bars');
-
-ativamenu.addEventListener('click',()=>{
-    ativamenu.classList.toggle('fa-x')
 
 })
     });
