a945d87a9fa66892ba31ee25cad551ef22a7b07f
Mauricio3697 <133618458+Mauricio3697@users.noreply.github.com>
2023-06-18 12:53:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,26 @@
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
         })
-        const titulo = document.querySelector('.digitando');
+  function ativaletra(elemento){
+    const arrTexto = elemento.innerHTML.split('')
+    elemento.innerHTML = '';
+    arrTexto.forEach((letra, i)=>{
+        setTimeout(()=>{
+            elemento.innerHTML+= letra;
+       }, 75 * i)
+    });
+    }
+ 
+    
+const titulo = document.querySelector('.digitando');
 ativaletra(titulo);
+
+const ativamenu = document.querySelector('.fa-bars');
+
+ativamenu.addEventListener('click',()=>{
+    ativamenu.classList.toggle('fa-x')
+
+})
     });
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
